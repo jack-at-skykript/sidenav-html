4254091b5ea84a6622083b4ab23888a596b9464a
Jack @ SkySkript <74991280+jack-at-skykript@users.noreply.github.com>
2020-11-24 17:54:13 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+function open() {
+  document.getElementById('sideNavigation').style.width = '20%';
+  document.getElementById('body').style.marginLeft = "20%";
+}
+function close() {
+  document.getElementById('sideNavigation').style.width = 0;
+  document.getElementById('body').style.marginLeft = 'none';
+}
